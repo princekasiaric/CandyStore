1c51d59c8aca1848d0fa9b39d26dfdab2f3bfbdf
Kasiaric <princekasiaric@gmail.com>
2020-02-13 15:59:30 -0800

    Created customized TagHelper for Contact Us

---
+++ CandyShop/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CandyShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
